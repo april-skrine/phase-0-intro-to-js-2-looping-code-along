04f9a3447bc69408b4deb5a11446d8d2b93e1a52
April Skrine <april.skrine@gmail.com>
2021-12-28 14:09:20 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+const eventName = "Birthday"
+
+function writeCards(names) {
+    const newNames = [...names];
+    for (let i = 0; i < newNames.length; i++) {
+        console.log(`Thank you ${names[i]} for the ${eventName} gift!`);
+    }
+
+    return newNames;
+}
+
+writeCards(names);
+
+
+
+
+let number = 10;
+
+function countDown(number) {
+    while (number >= 0) {
+    console.log(number--);}
+};
+
+countDown(number);
